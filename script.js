d93fdc09a44f0459931a7448d192adc9e452e074
rkoffice2124-coder <rkoffice2124@gmail.com>
2026-07-13 13:51:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function showSection(sectionId) {
 
+    document.getElementById("aboutSection").style.display = "none";
+    document.getElementById("whySection").style.display = "none";
+    document.getElementById("admissionSection").style.display = "none";
+
+    document.getElementById(sectionId).style.display = "block";
+
+    document.getElementById(sectionId).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+window.onload = function () {
+
+    document.getElementById("aboutSection").style.display = "none";
+    document.getElementById("whySection").style.display = "none";
+    document.getElementById("admissionSection").style.display = "none";
+
+};
